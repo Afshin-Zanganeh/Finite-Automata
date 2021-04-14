51e6e59ffc40e8599ca5c507a17a1dacc7067385
EmadMousavi <emadmoosavi79@gmail.com>
2021-04-14 18:49:53 +0430

    DFA.js and NFA.js changed in structure (NFA extends DFA) and draw function changed as one of DFA(parent class) methods

--- DFA.js
+++ DFA.js
@@ -73,9 +73,75 @@ class DFA
     return true
   }
 
-  ShowSchematicDFA()
+  ShowSchematic()
   {
-    draw();
+    this.draw();
+  }
+
+
+
+  
+  buildDotData(transitions, initialState, finalStates) {
+    let res = `digraph {
+      node [shape=circle fontsize=30]
+      edge [length=200, color=gray, fontcolor=black]`
+    transitions.forEach(element => {
+      element = element.split(",")
+      let from = element[0]
+      let to = element[1]
+      let label = element[2]
+      if (label == "") {
+        label = "lambda"
+      }
+      res += `
+        ${from} -> ${to}[label=${label}]`
+    });
+    if (finalStates[0] != "") {
+      finalStates.forEach(element => {
+        res += `
+          ${element} [
+            label=${element},
+            shape="doublecircle",
+            color="#ff0000",
+          ]`
+      });
+    }
+  
+    res += `
+    }`
+    return res
+  }
+  
+  
+  draw() {
+    let states = document.getElementById("States").value
+    states = states.split(",")
+    let initialState = states[0]
+    let alphabets = document.getElementById("Alphabet").value
+    alphabets = alphabets.split(",")
+    let finalStates = document.getElementById("FinalStates").value
+    finalStates = finalStates.split(",")
+    let numberOfTransitions = document.getElementById("NumberOfRules").value
+    let transitions = document.getElementById("GrammerRules").value
+    transitions = transitions.split("\n")
+  
+  
+    let dotData = this.buildDotData(transitions, initialState, finalStates)
+    let container = document.getElementById("graphContainer")
+    let data = vis.parseDOTNetwork(dotData)
+    let options = {
+      nodes: {
+        borderWidth: 2,
+        size: 50,
+        color: {
+          border: "#000000",
+          background: "#ffffff",
+        },
+        font: "1px arial black",
+      },
+    };
+    let network = new vis.Network(container, data, options)
+  
   }
 
 }
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function main()
   var data = document.forms["form"];
   var string = data["String"].value;
   var dfa = new DFA(data["states"].value.split(","), data["alphabet"].value.split(","), data["FinalStates"].value.split(","), data["GrammerRules"].value.split("\n"));
-  draw()
+  dfa.ShowSchematic();
   // console.log(dfa.graph);
   // alert(dfa.states);
   // alert(dfa.IsAcceptedByDFA(string));
